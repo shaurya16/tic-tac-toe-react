89fa71ca732f00a4c0173795304f07285064d50d
Shaurya Srivastava <shaurya16@gmail.com>
2025-10-17 01:24:59 +0800

    added edit button with save feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Player from "./components/Player";
+
 function App() {
   return (
     <main>
-      <h1>React Tic-Tac-Toe</h1>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
     </main>
   );
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(name);
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  let playerName = <span className="player-name">{inputValue}</span>;
+
+  if (isEditing) {
+    playerName = (
+      <input type="text" required value={inputValue} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
